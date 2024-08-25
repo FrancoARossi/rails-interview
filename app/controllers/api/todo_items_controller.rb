29c11ee95c4113a1166b601ade5b205d0a87be8d
FrancoARossi <rossifrancoa@gmail.com>
2024-08-25 13:36:59 -0300

    feat(items): finish api with new methods

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -1,25 +1,28 @@
 module Api
   class TodoItemsController < Api::ApplicationController
-    skip_before_action :verify_authenticity_token
+    before_action :set_todo_item, only: %i[show update destroy]
+    before_action :set_todo_list, only: %i[create index]
 
     def index
-      @todo_items = TodoItem.where(todo_list_id: params[:todo_list_id])
-
       respond_to do |format|
-        format.json { render json: @todo_items.to_json }
+        format.json { render_success(serialize_items(@todo_list.todo_items)) }
+      end
+    end
+
+    def show
+      respond_to do |format|
+        format.json { render_success(serialize_items(@todo_item)) }
       end
     end
 
     def create
-      @todo_list = TodoList.find(params[:todo_list_id])
-
       if @todo_list.todo_items.create(todo_item_params)
         respond_to do |format|
-          format.json { render json: @todo_list.todo_items.to_json }
+          format.json { render_success(serialize_items(@todo_list.todo_items.last), :created) }
         end
       else
         respond_to do |format|
-          format.json { render json: { error: @todo_item.errors } }
+          format.json { render_error(@todo_list.errors) }
         end
       end
     end
@@ -27,29 +30,23 @@ module Api
     def update
       if @todo_item.update(todo_item_params)
         respond_to do |format|
-          format.json { render json: @todo_item.to_json }
+          format.json { render_success(serialize_items(@todo_item)) }
         end
       else
         respond_to do |format|
-          format.json { render json: { error: @todo_item.errors } }
+          format.json { render_error(@todo_item.errors) }
         end
-      end
-    end
-
-    def show
-      respond_to do |format|
-        format.json { render json: @todo_item.to_json }
       end
     end
 
     def destroy
       if @todo_item.destroy
         respond_to do |format|
-          format.json { render json: @todo_item.to_json }
+          format.json { render_success(serialize_items(@todo_item)) }
         end
       else
         respond_to do |format|
-          format.json { render json: { error: @todo_item.errors } }
+          format.json { render_error(errors: @todo_item.errors) }
         end
       end
     end
@@ -60,8 +57,16 @@ module Api
       @todo_item = TodoItem.find(params[:id])
     end
 
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
     def todo_item_params
-      params.require(:todo_item).permit(:title, :description)
+      params.require(:todo_item).permit(:title, :description, :completed)
+    end
+
+    def serialize_items(todo_items)
+      todo_items.as_json(except: %i[todo_list_id updated_at created_at])
     end
   end
 end
